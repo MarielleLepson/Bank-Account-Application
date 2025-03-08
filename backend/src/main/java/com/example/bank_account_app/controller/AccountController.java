bacfc166bf4c93fef1f17190c07b06ae35b3164c
mariellelepson <marielle.lepson@ut.ee>
2025-03-08 23:37:12 +0200

    add swagger and first APIs

--- backend/src/main/java/com/example/bank_account_app/controller/AccountController.java
+++ backend/src/main/java/com/example/bank_account_app/controller/AccountController.java
@@ -1,14 +1,51 @@
 package com.example.bank_account_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bank_account_app.model.Account;
+import com.example.bank_account_app.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountRepository accountRepo;
 
+
+    /**
+     * Get all bank accounts.
+     */
     @GetMapping("/accounts")
-    public String getAccounts() {
-        return "List of accounts";
+    public ResponseEntity<?> getAllAccounts() {
+        log.info("Getting all accounts");
+        List<Account> accounts = accountRepo.findAll();
+
+        if (accounts.isEmpty()) {
+            log.warn("No bank accounts found");
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(accounts);
+    }
+
+    /**
+     * Get bank account by account number.
+     */
+    @PostMapping("/accounts/{accountNumber}")
+    public ResponseEntity<?> getAccountByNumber(@PathVariable String accountNumber) {
+        log.info("Getting account by account number: {}", accountNumber);
+        Account account = accountRepo.findByAccountNumber(accountNumber);
+
+        if (account == null) {
+            log.warn("Account not found: {}", accountNumber);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(account);
     }
 }
 
--- backend/src/main/java/com/example/bank_account_app/repository/AccountRepository.java
+++ backend/src/main/java/com/example/bank_account_app/repository/AccountRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    // TODO: ML-e: Add custom queries here
+    Account findByAccountNumber(String accountNumber);
 }
